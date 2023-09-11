f91e861d959a2e567741ad668dba5b2fa155728b
rickyshu <hoinleekk@naver.com>
2023-09-11 21:04:13 +0900

    feat: add calendar lib

--- storybook_practice/src/App.tsx
+++ storybook_practice/src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 function App() {
-  return null;
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div className="border border-2 border-slate-950 w-full h-full flex items-center justify-center">
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
 }
 
 export default App;
