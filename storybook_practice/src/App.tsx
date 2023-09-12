96b2f02dbee3387c77b3625a1f56ee517003a055
rickyshu <hoinleekk@naver.com>
2023-09-12 17:05:16 +0900

    feat: react calendar 틀 구현

--- storybook_practice/src/App.tsx
+++ storybook_practice/src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import { format } from "date-fns";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 function App() {
-  const [value, onChange] = useState<Value>(new Date());
+  const [date, setDate] = useState<Value>(new Date());
 
   return (
     <div className="border border-2 border-slate-950 w-full h-full flex items-center justify-center">
-      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        onChange={setDate}
+        value={date}
+        formatDay={(locale, date) => format(date, "d")}
+        locale="en-GB"
+        selectRange={true}
+        minDetail="month"
+      />
     </div>
   );
 }
--- storybook_practice/src/stories/Button.tsx
+++ storybook_practice/src/stories/Button.tsx
@@ -1,27 +1,11 @@
-import button from "../components/button";
 import "./button.css";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
@@ -39,16 +23,20 @@ const getSizeClasses = (size: ButtonProps["size"]) => {
   }
 };
 
-const getModeClasses = (isPrimary: boolean) => (isPrimary ? "text-white bg-pink-600 border-pink-600 dark:bg-pink-700 dark:border-pink-700" : "text-slate-700 bg-transparent border-slate-700 dark:text-white dark:border-white");
+const getModeClasses = (isPrimary: boolean) =>
+  isPrimary
+    ? "text-white bg-pink-600 border-pink-600 dark:bg-pink-700 dark:border-pink-700"
+    : "text-slate-700 bg-transparent border-slate-700 dark:text-white dark:border-white";
 
-//div class에 바로 셋팅을 하는 게 아니라, 아래처럼 변수로 지정을 하는 것!
-const BASE_BUTTON_CLASSES: string = "cursor-pointer rounded-full border-2 font-bold leading-none inline-block";
+const BASE_BUTTON_CLASSES: string =
+  "cursor-pointer rounded-full border-2 font-bold leading-none inline-block";
 
-/**
- * Primary UI component for user interaction
- */
-
-export const Button = ({ primary = false, size = "medium", label, ...props }: ButtonProps) => {
+export const Button = ({
+  primary = false,
+  size = "medium",
+  label,
+  ...props
+}: ButtonProps) => {
   const computedClasses = useMemo(() => {
     const modeClass = getModeClasses(primary);
     const sizeClass = getSizeClasses(size);
@@ -57,17 +45,12 @@ export const Button = ({ primary = false, size = "medium", label, ...props }: Bu
   }, [primary, size]);
 
   return (
-    <button type="button" className={`${BASE_BUTTON_CLASSES} ${computedClasses}`} {...props}>
+    <button
+      type="button"
+      className={`${BASE_BUTTON_CLASSES} ${computedClasses}`}
+      {...props}
+    >
       {label}
     </button>
   );
 };
-
-// export const Button = ({ primary = false, size = "medium", backgroundColor, label, ...props }: ButtonProps) => {
-//   const mode = primary ? "storybook-button--primary" : "storybook-button--secondary";
-//   return (
-//     <button type="button" className={["storybook-button", `storybook-button--${size}`, mode].join(" ")} style={{ backgroundColor }} {...props}>
-//       {label}
-//     </button>
-//   );
-// };
